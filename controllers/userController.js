7b9753a60cecaee18265b98d95bf4405cff89a86
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-08-15 12:38:02 +0530

    Handling Error's middlewares and added in Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,16 @@
+import ErrorHandler from '../utils/errorHandler.js'
 
+export const newUser = ( req, res , next ) => {
 
-export const newUser = ( req , res) => {
-    res.json({
-        message : "I'm Nilesh , Started the Express server !",
-    });
+    const userExist = true;
+
+    if ( userExist ) {
+        return next(new ErrorHandler("user Already Exist", 400))
+    }
+
+    res
+        .status(201)
+        .json({
+            message: "User Created Successfully !",
+        });
 }
