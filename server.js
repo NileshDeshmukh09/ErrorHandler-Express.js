44c8cd981a6de31fea369c99bbfd8beb04d888a8
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-08-15 12:21:19 +0530

    errorHandler Middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import errorMiddleware from './middlewares/Error.js';
 import userRoutes from './routes/user.model.js';
 const app = express();
 const PORT = 4000;
@@ -7,4 +8,6 @@ app.use("/user" , userRoutes);
 
 app.listen( PORT , ()=> {
     console.log(`Server is Running on http://localhost:${PORT}`)
-})
+});
+
+app.use( errorMiddleware );
